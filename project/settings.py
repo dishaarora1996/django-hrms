dbf4b292724f186a095ec5dd6dfddc7d1b49dacc
Disha Arora <disha.arora@navsoft.in>
2023-08-28 10:34:51 +0530

    Database update to sqite

--- project/settings.py
+++ project/settings.py
@@ -80,24 +80,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'db1',
-       'USER': 'postgres',
-       'PASSWORD': 'Navsoft1234@',
-       'HOST': '44.212.136.38',
-       'PORT': '5432',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
+
 
 
 REST_FRAMEWORK = {
